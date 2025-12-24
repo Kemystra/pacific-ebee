dc00f97f7d5267c713cbdf164b34dd2fdd252236
Ying Tong <yingtong0106@gmail.com>
2025-12-24 11:36:13 +0800

    add return types and parameters for methods

--- src/server.cpp
+++ src/server.cpp
@@ -1,23 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-class ServerConnection {
-    void startConnection() {
-        cout << "Connection started";
-    }
-}
-
 class Connection {
-    void sendMessage() {
-        cout << "Message is being sent by Connection class";
+    private:
+        int socketFD; 
+
+public:
+    Connection(int fd) : socketFD(fd) {}
+
+    ~Connection() {
+        cout << "Destructor for Connection executing...";
     }
 
-    void listenForMessage() {
-        cout << "Connection class is listening for messages";
+    void sendMessage(string message) {
+        cout << "Sending to FD " << socketFD << ": " << message << endl;
     }
-}
+
+    string listenForMessage() {
+        cout << "Listening on FD " << socketFD << "..." << endl;
+        return "Stub Message"; 
+    }
+
+    int getFD() { return socketFD; }
+};
+
+class ServerConnection {
+private:
+    int port = 8000;
+    int serverFD;
+
+public:
+    ServerConnection(int portNum) : port(portNum) {}
+
+    void startConnection() {
+        cout << "Server started on port " << port << endl;
+    }
+
+};
 
 int main (int argc, char *argv[]) {
-    
+
     return 0;
 }
