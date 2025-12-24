ac0d96b0a8d541baf7913af514d2ca0e2abe0e69
Ying Tong <yingtong0106@gmail.com>
2025-12-24 11:29:02 +0800

    feat: Added stubs for ServerConnection and Connection classes

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class ServerConnection {
+    void startConnection() {
+        cout << "Connection started";
+    }
+}
+
+class Connection {
+    void sendMessage() {
+        cout << "Message is being sent by Connection class";
+    }
+
+    void listenForMessage() {
+        cout << "Connection class is listening for messages";
+    }
+}
+
 int main (int argc, char *argv[]) {
     
     return 0;
