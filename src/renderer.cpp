65366a0c131e4f1015ba9ac95d3843a95155f0d2
Jax Choong <choongjax@gmail.com>
2025-12-24 11:56:23 +0800

    chore: added inline doc comments and added missing semicolon

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,30 +8,33 @@ class Renderer{
     private:
         stringstream consoleBuffer;
     public:
-        Renderer();
-        ~Renderer();
         void log(const string& message){
-            // use this if logging msg has no color
+            // prints normally if passed a normal message,
+            // prints colored stuff if passed consoleBuffer
             cout << message << endl;
         };
         void error(const string& message){
             string errorMessage = "Error: " + message;
-            outputToConsole(errorMessage);
+            log(errorMessage);
         };
         void setBufferColor(const char* color){
+            cout << "setting buffer color to: " << color;
             consoleBuffer << color;
         };
         void resetBufferColor(){
+            cout << "reset buffer color to default";
             consoleBuffer << "\033[0m"; // Reset color to default
         };
         void bufferedLog(const string& message){
             // add msg to console buffer with the current color
+            cout << "Adding message to consoleBuffer." <<endl;
             consoleBuffer << message;
         };
         void flushBufferedLog(){
+            cout << "Flushing buffered log" << endl;
             // print all the colored stuff to console, and clear it after
-            if (consoleBuffer.str().empty()||fileBuffer.str().empty()) return; // No buffered log to flush
-            outputToConsole(consoleBuffer.str());
+            if (consoleBuffer.str().empty()) return; // No buffered log to flush
+            log(consoleBuffer.str());
             consoleBuffer.str(""); // Clear the buffer after flushing
         };
 };
