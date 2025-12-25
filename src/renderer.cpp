ea40eb7d171eb0bc245280d9c9860b13b3dfecbc
Jax Choong <choongjax@gmail.com>
2025-12-25 13:49:08 +0800

    feat: added ansi escape codes

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,34 +8,25 @@ class Renderer{
     private:
         stringstream consoleBuffer;
     public:
+        // ansi escape codes for colours, blinking and bold, as well as resetting shtuff
+        const char* BLUE_COLOR = "\033[34m";
+        const char* RESET_COLOR = "\033[0m";
+        const char* ORANGE_COLOR = "\033[33m";
+        const char* RED_COLOR = "\033[31m";
+        const char* SLOW_BLINK = "\033[5m";
+        const char* RAPID_BLINK = "\033[6m";
+        const char* STOP_BLINK = "\033[25m";
+        const char* BOLD = "\033[1m";
+        const char* NORMAL_WEIGHT = "\033[22m";
+        const char* RESET_ATTRIBUTES = "\033[0m";
+
         void log(const string& message){
             // prints normally if passed a normal message,
             // prints colored stuff if passed consoleBuffer
             cout << message << endl;
         };
-        void error(const string& message){
-            string errorMessage = "Error: " + message;
-            log(errorMessage);
-        };
-        void setBufferColor(const char* color){
-            cout << "setting buffer color to: " << color;
-            consoleBuffer << color;
-        };
-        void resetBufferColor(){
-            cout << "reset buffer color to default";
-            consoleBuffer << "\033[0m"; // Reset color to default
-        };
-        void bufferedLog(const string& message){
-            // add msg to console buffer with the current color
-            cout << "Adding message to consoleBuffer." <<endl;
-            consoleBuffer << message;
-        };
-        void flushBufferedLog(){
-            cout << "Flushing buffered log" << endl;
-            // print all the colored stuff to console, and clear it after
-            if (consoleBuffer.str().empty()) return; // No buffered log to flush
-            log(consoleBuffer.str());
-            consoleBuffer.str(""); // Clear the buffer after flushing
-        };
+        void render(char[] field){
+            cout << field;
+        }
 };
 
