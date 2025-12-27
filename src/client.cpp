ab276cfd9607d59ed9f24f6743fcbbad874d74c2
k3mystra <programmer.coleslaw981@slmail.me>
2025-12-27 22:16:08 +0800

    feat: Initial addition to client's main()

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+
+
+using namespace std;
+
 int main (int argc, char *argv[]) {
-    
+    // Get the actual first argument (IP address of server)
+    // Note that the first argument (argv[0]) is the program name
+    const char* serverIP = argv[1];
+
     return 0;
 }
